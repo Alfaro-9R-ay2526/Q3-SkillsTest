61602dd9dd9119359736fd1b15804ba63f6f0938
Alfaro-9R-ay2526 <alfaro.ac@obmontessori.edu.ph>
2026-03-13 13:56:04 +0800

    Implement total calculation in main.js
    Added a function to calculate total based on ticket price and admin fee.

---
+++ main.js
@@ -0,0 +1,22 @@
+total= Admin fee + Subtotal
+
+Subtotal 250 * 3
+
+GIVEN:
+
+Ticket = 250
+Admin Fee = 50
+
+Display: Total
+
+
+Display:WebTransportBidirectionalStream
+
+Function getTotal(){
+    //declare variables
+    const AdminF = 50,
+    const TICKET_PRICE = 250
+
+    let SUBT = AdminF * TICKET_PRICE;
+    let Total = SUBT + QTY
+    let QTY = document.getElementById("Total").Value
